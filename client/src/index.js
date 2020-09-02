a4edeb867bda42828047f46f19887094b644b947
rodgersea <elliottarodgers@gmail.com>
2020-09-01 20:01:43 -0400

    early

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-w6ngsgg0.us.auth0.com"
+    clientId="7P4kuhZrJEpYztxQptzCUUwOd6ngVb6r"
+    redirectUri="http://localhost:3000/"
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
